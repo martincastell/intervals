e8f150b12f30861c9a9e817688324883807572d6
Martin Castellanos <martin@sumologic.com>
2018-04-09 21:45:25 -0700

    First pass at wiring everything together.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react';
 import 'normalize.css';
 import './App.css';
+import {getRunningIntervalState, pause, reset, resume, setRoundsTo, setRoundTimeTo, start} from './state/appState';
 import IntervalConfig from './components/IntervalConfig/IntervalConfig';
-import {setRoundsTo, setRoundTimeTo} from './state/appState';
 
 class App extends Component {
 
   state = {
     config: {
       rounds: 10,
-      roundTime: 60,
+      roundTime: 10,
     },
+    request: null,
+    // interval instance...
+    startTime: null,
+    pauseTime: null,
+    isRunning: false,
   };
 
+  start = () => {
+    this.tick();
+    this.setState(start);
+  };
+  pause = () => {
+    this.cancelTick();
+    this.setState(pause);
+  };
+  resume = () => {
+    this.tick();
+    this.setState(resume)
+  };
+  reset = () => {
+    this.cancelTick();
+    this.setState(reset)
+  };
   onRoundsChange = (event) => this.setState(setRoundsTo(event.target.value));
   onRoundTimeChange = (event) => this.setState(setRoundTimeTo(event.target.value));
 
+  tick = () => this.setState({ request: requestAnimationFrame(this.tick) });
+  cancelTick = () => {
+    cancelAnimationFrame(this.state.request);
+    this.setState({ request: null});
+  };
+
   render() {
-    const {config} = this.state;
-    const {onRoundsChange, onRoundTimeChange} = this;
+    const {config, isRunning, startTime, pauseTime} = this.state;
+    const interval = getRunningIntervalState(this.state);
 
     return (
       <div className="App">
         <IntervalConfig config={config}
-                        onRoundsChange={onRoundsChange}
-                        onRoundTimeChange={onRoundTimeChange} />
+                        onRoundsChange={this.onRoundsChange}
+                        onRoundTimeChange={this.onRoundTimeChange} />
+        <div>
+          <button type="button" onClick={this.start}>Start</button>
+          <button type="button" onClick={this.pause}>Pause</button>
+          <button type="button" onClick={this.resume}>Resume</button>
+          <button type="button" onClick={this.reset}>Reset</button>
+        </div>
+        <div>isRunning: {isRunning}</div>
+        <div>startTime: {startTime}</div>
+        <div>pauseTime: {pauseTime}</div>
+        <div>Current Round: {interval.round}</div>
+        <div>Current Round Remaining: {interval.roundRemaining}</div>
       </div>
     );
   }
--- src/state/appState.js
+++ src/state/appState.js
@@ -12,3 +12,54 @@ export function setRoundsTo(rounds) {
 export function setRoundTimeTo(roundTime) {
   return (state) => ({ config: updateConfig(state.config, {roundTime}) });
 }
+
+export function start() {
+  return {
+    isRunning: true,
+    startTime: Date.now(),
+  };
+}
+
+export function reset() {
+  return {
+    isRunning: false,
+    startTime: null,
+    pauseTime: null,
+  };
+}
+
+export function pause() {
+  return {
+    isRunning: false,
+    pauseTime: Date.now(),
+  };
+}
+
+export function resume({pauseTime, startTime}) {
+  const now = Date.now();
+
+  return {
+    isRunning: true,
+    startTime: (now - pauseTime) + startTime,
+    pauseTime: null,
+  };
+}
+
+export function getRunningIntervalState({isRunning, config, startTime, pauseTime}) {
+  if (startTime) {
+    const now = Date.now();
+    const totalElapsed = ((pauseTime || now) - startTime) / 1000;
+    const round = Math.floor(totalElapsed/config.roundTime) + 1;
+    const roundRemaining = round * config.roundTime - totalElapsed;
+
+    return {
+      round,
+      roundRemaining: roundRemaining.toFixed(1),
+    }
+  } else {
+    return {
+      round: 1,
+      roundRemaining: config.roundTime,
+    }
+  }
+}
